d351a766096ab518912723c478a057cdea1e31e0
duwlsssss <rladuwls0814@gmail.com>
2024-11-26 12:36:56 +0900

    feat: not-found 페이지 추가 (#21)

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export { Home } from './Home/Home';
+export { Login } from './Login/Login';
+export { Register } from './Register/Register';
+export { Profile } from './Profile/Profile';
+export { SalaryDetails } from './salary-details/SalaryDetails';
+export { CorrectionRequest } from './correction-request/CorrectionRequest';
+export { Calendar } from './Calendar/Calendar';
+export { NotFound } from './not-found/NotFound';
