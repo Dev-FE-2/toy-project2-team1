ec430e46d9f00bf0915a68455f0f9a964b816af8
rondido <rppr001@naver.com>
2024-12-09 17:49:56 +0900

    feat:관리자 직원 일정 관리 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '@/layout/Layout';
+import { ProtectedRoute } from '@/components/auth/ProtectedRoute';
 import {
 	Home,
 	Login,
 	Register,
-	Profile,
 	SalaryDetails,
 	CorrectionRequest,
 	ScheduleManagement,
@@ -20,10 +20,38 @@ export const router = createBrowserRouter([
 			{ path: '/', element: <Home /> },
 			{ path: '/login', element: <Login /> },
 			{ path: '/register', element: <Register /> },
-			{ path: '/profile', element: <Profile /> },
-			{ path: '/salary-details', element: <SalaryDetails /> },
-			{ path: '/correction-request', element: <CorrectionRequest /> },
-			{ path: '/schedule-management', element: <ScheduleManagement /> },
+			{
+				path: '/profile',
+				element: (
+					<ProtectedRoute>
+						<></>
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: '/salary-details',
+				element: (
+					<ProtectedRoute>
+						<SalaryDetails />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: '/correction-request',
+				element: (
+					<ProtectedRoute>
+						<CorrectionRequest />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: '/schedule-management',
+				element: (
+					<ProtectedRoute>
+						<ScheduleManagement />
+					</ProtectedRoute>
+				),
+			},
 		],
 	},
 ]);
